80241be430f649a08359f3909f19640e5b3a4181
e <e@encyclopediadramatica.se>
2012-12-03 06:45:37 -0500

    rename some variable

--- MiniBin.py
+++ MiniBin.py
@@ -20,12 +20,12 @@ app = Flask(__name__)
 file_path = '/tmp/minibin'
 
 domain = string.ascii_letters + string.digits + "_-"
-rString = lambda len = 15: ''.join(random.choice(domain) for i in xrange(len))
+random_filename = lambda len = 15: ''.join(random.choice(domain) for i in xrange(len))
 domain2 = string.ascii_letters + string.digits
 random_prefix = lambda len = 1: ''.join(random.choice(domain2) for i in xrange(len))
 
 if not os.path.exists(file_path):
-    os.makedirs(file_path, mode = 0777)
+    os.makedirs(file_path)
 
 if not os.path.isdir(file_path):
     raise RuntimeError("%r is not a directory." % file_path)
@@ -42,13 +42,13 @@ def up():
     code = request.form['code']
     prefix_1 = random_prefix()
     prefix_2 = random_prefix(2)
-    file_name = rString(12)
+    file_name = random_filename(12)
     date_str = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
 
     _file_path = os.path.join(file_path, prefix_1, prefix_2)
 
     if not os.path.exists(_file_path):
-        os.makedirs(_file_path, 0777)
+        os.makedirs(_file_path)
 
     with open(os.path.join(_file_path, file_name), 'w') as fp:
         fp.write(render_template("paste.html", date_str = date_str, code = code))
